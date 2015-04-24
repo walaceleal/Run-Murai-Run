cdc41669ee9778cfa8ba7c045086a750971dcc7e
walaceleal <walaceleal.nave@gmail.com>
2015-04-24 11:36:19 -0300

    gerenciador de cenas

---
+++ Assets/Codigo/cenario.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SerializePrivateVariables]
+public class cenario : MonoBehaviour {
+    private float velocidade;
+    private static Vector2 posicaoInicial;
+
+    private bool emMovimento;
+    public tipo_cenario tipo;
+
+	void Start () {
+        posicaoInicial = Camera.main.ScreenToWorldPoint(new Vector2(2 * Screen.width, 0));
+        posicaoInicial = new Vector2(posicaoInicial.x, 0);
+	}
+	
+	void Update () {
+        if (emMovimento)
+        {
+            transform.Translate(-Vector2.right * velocidade * Time.deltaTime);
+
+            if (SaiuTela())
+            {
+                emMovimento = false;
+            }
+        }
+	}
+
+    bool SaiuTela()
+    {
+        float x = Camera.main.WorldToScreenPoint(transform.position).x;
+
+        return x < -Screen.width;// || x > Screen.width;
+    }
+
+    public void reset(){
+        emMovimento = true;
+        transform.position = posicaoInicial;
+    }
+
+    public bool estaMovimento()
+    {
+        return emMovimento;
+    }
+}
+
+public enum tipo_cenario
+{
+    casa_1 = 0,
+    casa_2 = 1,
+    case_3 = 2,
+    casa_4 = 3,
+    casa_5 = 4
+};
---
+++ Assets/Codigo/gerenciador_cenario.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SerializePrivateVariables]
+public class gerenciador_cenario : MonoBehaviour {
+
+    public cenario[] casas;
+    private cenario[,] piscina;
+    private float tempo, tempoCriacao;
+    private int tamanho;
+
+	void Start () {
+
+        piscina = new cenario[casas.GetLength(0), tamanho];
+
+        for (int i = 0; i < casas.GetLength(0); i++)
+        {
+            for(int j = 0; j < tamanho; j++){
+                piscina[i, j] = Instantiate(casas[i]);
+            }
+        }
+	}
+
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        if (tempo >= tempoCriacao)
+        {
+            if (add_casa())
+            {
+                tempo -= tempoCriacao;
+            }
+        }
+	}
+
+    bool add_casa()
+    {
+        int r = Random.Range(0, piscina.GetLength(0));
+        
+        for (int i = 0; i < piscina.GetLength(1); i++)
+        {
+            if (!piscina[r, i].estaMovimento())
+            {
+                piscina[r, i].reset();
+                return true;
+            }
+        }
+        return false;
+    }
+}
